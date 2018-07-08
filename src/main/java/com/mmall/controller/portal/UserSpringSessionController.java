1f5f703530b1ea0fac2f473f2458117629c002c5
db <978295205@qq.com>
2018-07-08 12:55:01 +0800

    spring session

---
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,69 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServiceResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.Session;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 使用spring session框架来实现单点登陆
+ */
+@Controller
+@RequestMapping("/user/springSession/")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @RequestMapping(value = "login.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
+        ServiceResponse<User> response = iUserService.login(username,password);
+        if(response.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    /**
+     * 退出登陆，如果获取的loginToken为空，则表示未登录，同样提示退出成功
+     * @param session
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "logout.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<String> logout(HttpSession session,HttpServletResponse response){
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServiceResponse.createBySuccessMessage("成功退出登陆");
+    }
+
+
+    @RequestMapping(value = "get_user_info.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<User> getUserInfo(HttpSession session){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user != null){
+            return ServiceResponse.createBySuccess(user);
+        }
+
+        return ServiceResponse.createByErrorMessage("用户当前未登录，无法获取详细信息");
+    }
+
+
+}
